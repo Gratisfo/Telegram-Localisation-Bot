9113f656019d80b486ec57bdd308e62da87a3b4c
Gratisfo <31654704+Gratisfo@users.noreply.github.com>
2022-05-16 23:04:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -26,39 +26,44 @@ def save_author_data(message):
     col = f'{lang_col[message.text]}2'
     id_author = message.chat.id
     wks_authors.update(col, id_author)
-    bot.send_message(message.chat.id, "Please wait the new texts")  # change phrase there
+    bot.send_message(message.chat.id, "Please wait the new texts (: We will send it to you here.")  # change phrase there
 
 info = []
 
 @bot.callback_query_handler(func=lambda call: True)
 def test_callback(call):
     ids = wks_authors.row_values(2)
+    print(ids)
     ids_dict = {ids[3]: 'D', ids[2]: 'C', ids[4]: 'E', ids[1]: 'B'}
     user_id = call.message.chat.id
     row = str(call.data[-1])
     col = ids_dict[str(user_id)]
     info.insert(0, col + row)
+    markup = ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
+    markup.add("Cancel")
     if call.data[:4] == 'text':
-        msg = bot.send_message(user_id, 'Please send a translation of the text in reply message then it will be ready.')
+        msg = bot.send_message(user_id, 'Please send a translation of the text in reply message then it will be ready.', reply_markup=markup)
         bot.register_next_step_handler(msg, save_trans_text)
     elif call.data[:4] == 'link':
-        msg = bot.send_message(user_id, 'Please send a link to Google doc with translation of the text in reply message then it will be ready.')
+        msg = bot.send_message(user_id, 'Please send a link to Google doc with translation of the text in reply message then it will be ready.', reply_markup=markup)
         bot.register_next_step_handler(msg, save_trans_link)
 
 
 def save_trans_text(message):
-    translation = message.text
-    path = info[0]
-    print(translation, path)
-    wks_texts.update(path, translation)
-    bot.send_message(message.chat.id, 'Thank you very much!')
+    if message.text != "Cancel":
+        translation = message.text
+        path = info[0]
+        print(translation, path)
+        wks_texts.update(path, translation)
+        bot.send_message(message.chat.id, 'Thank you very much!')
 
 def save_trans_link(message):
-    link = message.text
-    path = info[0]
-    print(link, path)
-    wks_academy.update(path, link)
-    bot.send_message(message.chat.id, 'Thank you very much!')
+    if message.text != "Cancel":
+        link = message.text
+        path = info[0]
+        print(link, path)
+        wks_academy.update(path, link)
+        bot.send_message(message.chat.id, 'Thank you very much!')
 
 
 bot.polling(non_stop=True)
